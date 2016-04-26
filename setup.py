7e3e2a6e618392f75fe2c39f03c081f188e748dd
zhengxiaowai <h1x2y3awalm@gmail>
2016-04-26 18:08:03 +0800

    upload pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'django-bind-middleware',
+    packages = ['django_bind_middleware'],
+    license = 'MIT',
+    version = '0.1',
+    description = 'bind json data to request.',
+    author = 'zhengxiaowai',
+    author_email = 'h1x2y3awalm@gmail.com',
+    url = 'https://github.com/zhengxiaowai/django-bind-middleware/',
+    keywords = ['django','bind','request'],
+    classifiers = [],
+)
